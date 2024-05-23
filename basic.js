2b6a957f067b4517585c6d34bb5577287e8e40f2
mucsinyusuf <muxsinyussuf@gmail.com>
2024-05-23 05:14:22 -0400

    learning basics

--- basic.js
+++ basic.js
@@ -1,5 +1,51 @@
-// Define your name
-var myName = "Mucsin";
+//output
+console.log ("Hello world");
+//declaring variables
+//data types
+// string
+//number
+//boolean
+//null
 
-// Log your name to the console
-console.log("My name is: " + myName);
+let number = 60;
+const text = "hello"; // we use const when we know the value of the value wont change
+var lightsOn = 
+// var is outdated so we should use let and const
+console.log(number)
+
+// type conversion
+// we can change the data type of a variable
+let cnumber = '' ;
+
+cnumber = Number ('55');
+console.log(cnumber)
+console.log(typeof cnumber)
+
+// we can also use toString();
+let mynumber= 25;
+let mystring = mynumber.toString()
+console.log(typeof mystring)
+
+// We can also use the parseInt() and parseFloat() functions:
+// We use parseInt() for whole numbers and parseFloat() for numbers with decimal places.
+let munmber= 100.25;
+let yous = parseInt(munmber)
+console.log(yous)
+
+
+// You are given two variables of type String. 
+//The variables are string1 and string2. 
+//They are already declared have a string value like '52'.
+// You can already use them without worrying about declaring them.
+// Your job is to convert them into numbers and calculate their sum. 
+//Print the sum to complete the program.
+
+
+
+string1 = '52'
+string2 = '52'
+
+let sum = Number(string1) + Number(string2)
+console.log(sum)
+
+
